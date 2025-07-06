0856d7f57f2334cfb7254e92fbc0581ab2abb8fd
Sarah Sporck <sarah.sporck@student.uni-augsburg.de>
2025-07-06 18:59:28 +0200

    configure gh pages

--- src/components/tf-main.vue
+++ src/components/tf-main.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { NList, NListItem } from 'naive-ui'
+import { NList, NListItem, NThing } from 'naive-ui'
 </script>
 <template>
   <n-thing>{{ $t('home.introduction') }}</n-thing>
