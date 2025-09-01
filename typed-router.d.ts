d0e0228db79b47a38f2e781e572b579b49323de1
PhilippBraml <bramlphilipp@outlook.com>
2025-09-01 22:08:38 +0200

    Addition of new announcement page and small adjusments for mobile and translation

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -20,6 +20,7 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/about': RouteRecordInfo<'/about', '/about', Record<never, never>, Record<never, never>>,
+    '/announcements': RouteRecordInfo<'/announcements', '/announcements', Record<never, never>, Record<never, never>>,
     '/events': RouteRecordInfo<'/events', '/events', Record<never, never>, Record<never, never>>,
   }
 }
