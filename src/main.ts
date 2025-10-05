b273a8030a6a9f6cf2b128ffd11fe2f3b6965b18
PhilippBraml <bramlphilipp@outlook.com>
2025-10-05 21:05:31 +0200

    Adding instagram in iframe for news

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,14 @@ import App from './App.vue'
 import i18n from './i18n'
 import router from './router'
 
+import InstagramFeed from 'vue3-instagram-feed'
+import 'vue3-instagram-feed/dist/style.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+app.use(InstagramFeed)
 
 app.mount('#app')
