fc9bd1b21119954baa87f416455679e80f305251
PhilippBraml <bramlphilipp@outlook.com>
2025-10-08 15:27:36 +0200

    Fix for img routing

--- src/components/tf-main.vue
+++ src/components/tf-main.vue
@@ -45,7 +45,7 @@ const items: { tcg?: string; src: string; alt: string }[] = [
       style="justify-content: center; width: 100%; padding: 0"
     >
       <RouterLink
-        :to="{ name: '/announcment', query: { tcg: item.tcg } }"
+        :to="{ name: '/calendar', query: { tcg: item.tcg } }"
         style="width: 100%; height: 100%; display: flex; justify-content: center"
       >
         <NImage
